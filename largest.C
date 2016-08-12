e8d521aecbc972c0ea3765c3263b9accb58a8a30
premkumar97 <premkumar97@users.noreply.github.com>
2016-08-12 15:33:51 +0530

    Add files via upload

---
+++ largest.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int n,dig,size=0,i,j,temp,ans=0;
+int sol[100];
+clrscr();
+printf("Enter the number");
+scanf("%d",&n);
+printf("Enter the Value of limit");
+scanf("%d",&dig);
+for (j = 0;n>0; j++)
+{
+sol[j]=n%10;
+n=n/10;
+size++;
+}
+for(i=0;i<size;i++)
+{
+for(j=i;j<size;j++)
+{
+if(sol[i]>sol[j])
+{
+ temp=sol[j];
+ sol[j]=sol[i];
+ sol[i]=temp;
+}
+}
+}
+for(i=0;i<size-dig;i++)
+{
+ ans=ans*10+sol[i];
+}
+printf("%d",ans);
+}
