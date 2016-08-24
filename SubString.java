6a4d439d0f02cfb0938cba8d3eb9535a213b091d
premkumar97 <premkumar97@users.noreply.github.com>
2016-08-24 10:52:25 +0530

    Create SubString.java

---
+++ SubString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SubString {
+public static void main(String args[])
+{
+	Scanner sn=new Scanner(System.in);
+	String a=sn.nextLine();
+	System.out.println(a.indexOf("abcd"));
+	
+}
+}
