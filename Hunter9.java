b0b1e7fcb05ed4a678c2d05f983a4195f3ffd84d
premkumar97 <premkumar97@users.noreply.github.com>
2016-08-31 11:35:10 +0530

    Add files via upload

---
+++ Hunter9.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Hunter9 {
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		int lim=scan.nextInt();
+		int num[]=new int[lim+1];
+		int i,j,small,tot=0,num1=0,num2=0;
+		for(i=0;i<lim;i++)	
+		{
+			num[i]=scan.nextInt();
+		}
+		small=num[0]-num[1];
+		for(i=0;i<lim;i++)
+		{
+			tot=0;
+			for(j=i+1;j<lim;j++)
+			{
+				tot=(num[j]>0)?num[i]-num[j]:num[i]+num[j];
+				tot=tot<0?-tot:tot;
+				
+				if(small>tot)
+				{
+				
+					small=tot;
+					num1=num[i];
+					num2=num[j];
+				}
+			}
+		}
+		System.out.println(+num1+" "+num2);
+	}
+
+}
