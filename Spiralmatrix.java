ac95361538ae55568d90cfe23bd275982b9ee509
premkumar97 <premkumar97@users.noreply.github.com>
2017-01-24 14:57:23 +0530

    Add files via upload

---
+++ Spiralmatrix.java
@@ -0,0 +1,79 @@
+package prem;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Spiralmatrix {
+	public static void main(String args[]){
+		Spiralmatrix s=new Spiralmatrix();
+		
+		Scanner a= new Scanner(System.in);
+		int row=a.nextInt();
+		int val[][] = new int[row][row];
+		
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<row;j++)
+			{
+				val[i][j]= a.nextInt();
+			}
+		}
+		System.out.println(s.spiralOrder(val));
+	}
+    public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+ 
+        if(matrix == null || matrix.length == 0) return result;
+ 
+        int m = matrix.length;
+        int n = matrix[0].length;
+ 
+        int x=0; 
+        int y=0;
+ 
+        while(m>0 && n>0){
+ 
+            //if one row/column left, no circle can be formed
+            if(m==1){
+                for(int i=0; i<n; i++){
+                    result.add(matrix[x][y++]);
+                }
+                break;
+            }else if(n==1){
+                for(int i=0; i<m; i++){
+                    result.add(matrix[x++][y]);
+                }
+                break;
+            }
+ 
+            //below, process a circle
+ 
+            //top - move right
+            for(int i=0;i<n-1;i++){
+                result.add(matrix[x][y++]);
+            }
+ 
+            //right - move down
+            for(int i=0;i<m-1;i++){
+                result.add(matrix[x++][y]);
+            }
+ 
+            //bottom - move left
+            for(int i=0;i<n-1;i++){
+                result.add(matrix[x][y--]);
+            }
+ 
+            //left - move up
+            for(int i=0;i<m-1;i++){
+                result.add(matrix[x--][y]);
+            }
+ 
+            x++;
+            y++;
+            m=m-2;
+            n=n-2;
+        }
+ 
+        return result;
+    }
+}
